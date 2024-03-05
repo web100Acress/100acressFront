cd52ee3bae805ec8efa8efb70c87c3eceb32c70f
web100Acress <158815978+web100Acress@users.noreply.github.com>
2024-03-05 18:50:36 +0530

    Update Cities.jspic

--- src/Components/HomePageComponents/Cities.js
+++ src/Components/HomePageComponents/Cities.js
@@ -127,7 +127,7 @@ const Wrapper = styled.section`
   }
 
   .bg-one {
-    background-image: url(../../OtherImages/Sohnaroad.png);
+    background-image: url(../../OtherImages/Sohnaroad.PNG);
     background-position: center;
   }
 
@@ -137,7 +137,7 @@ const Wrapper = styled.section`
   }
 
   .bg-three {
-    background-image: url(../../OtherImages/mgRoad.png);
+    background-image: url(../../OtherImages/mgRoad.PNG);
     background-position: center;
   }
 
