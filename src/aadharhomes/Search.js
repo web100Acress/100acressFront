0c1ce7196eeb2d8e0bc43932b589038e34797c36
100acress <seo.100acress@gmail.com>
2024-09-22 18:09:29 +0530

    thirdcommit22sept2024

--- src/aadharhomes/Search.js
+++ src/aadharhomes/Search.js
@@ -1,3 +1,144 @@
+// import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+// const Search = ({ data1 }) => {
+//   const [formData, setFormData] = useState({
+//     location: "",
+//     query: "",
+//     collectionName: data1,
+//   });
+
+//   const primeLocations = [
+//     { name: "Golf Course Road", to: "/property-in-gurugram/golf-course/" },
+//     { name: "NPR", to: "/property-in-gurugram/northern-peripheral-road/" },
+//     {
+//       name: "Dwarka Expressway",
+//       to: "/property-in-gurugram/dwarka-expressway/",
+//     },
+//     { name: "SPR", to: "/property-in-gurugram/southern-peripheral-road/" },
+//     { name: "NH-48", to: "/property-in-gurugram/nh-48/" },
+//     {
+//       name: "Golf Course Extn Road",
+//       to: "/property-in-gurugram/golf-course-extn-road/",
+//     },
+//     {
+//       name: "New Gurgaon",
+//       to: "/property-in-gurugram/new-gurgaon/",
+//     },
+//   ];
+
+//   useEffect(() => {
+//     setFormData((prevState) => ({
+//       ...prevState,
+//       collectionName: data1,
+//     }));
+//   }, [data1]);
+
+//   const handleInputChange = (event) => {
+//     const { name, value } = event.target;
+//     setFormData({
+//       ...formData,
+//       [name]: value,
+//     });
+//   };
+
+//   const handleKeyPress = (event) => {
+//     if (event.key === "Enter") {
+//       document.getElementById("searchButton").click();
+//     }
+//   };
+
+//   const placeholders = [
+//     'Search "Villas"',
+//     'Search "3 BHK Ready To Move Flat For Sale In Gurgaon"',
+//     'Search "Best Properties"',
+//     'Search "Delhi NCR"',
+//     'Search "3 BHK Sale For Goa"',
+//     'Search "Commercial Space For Sale In Gurgaon"',
+//     // Add more placeholders as needed
+//   ];
+
+//   const [currentPlaceholder, setCurrentPlaceholder] = useState(placeholders[0]);
+
+//   useEffect(() => {
+//     const intervalId = setInterval(() => {
+//       setCurrentPlaceholder((prev) => {
+//         const currentIndex = placeholders.indexOf(prev);
+//         const nextIndex = (currentIndex + 1) % placeholders.length;
+//         return placeholders[nextIndex];
+//       });
+//     }, 3000);
+
+//     return () => clearInterval(intervalId);
+//   }, []);
+
+//   return (
+//     <>
+//       <div
+//         className="w-70 shadow-xl bg-white border-none lg:h-14 md:h-10 sm:h-8 px-2 lg:px-4 md:px-3 sm:px-2"
+//         style={{ marginTop: window.innerWidth < 640 ? "200px" : "0" }}
+//       >
+//         <div className="flex items-center xl:h-14 lg:h-14 md:h-10 sm:h-8">
+//           <div className="w-60 mt-1 lg:mt-3 md:mt-3 sm:mt-12 ml-2 lg:ml-8 md:ml-6 sm:ml-4 lg:w-[620px] md:w-full sm:w-70">
+//             <input
+//               className="outline-none w-full"
+//               type="text"
+//               name="query"
+//               placeholder={currentPlaceholder}
+//               value={formData.query}
+//               onChange={handleInputChange}
+//               onKeyPress={handleKeyPress}
+//             />
+//           </div>
+
+//           <Link
+//             to={{
+//               pathname: `/searchdata/${encodeURIComponent(
+//                 JSON.stringify(formData)
+//               )}`,
+//               state: formData,
+//             }}
+//             id="searchButton"
+//             className="ml-2 my-1 mt-1 lg:mt-3 md:mt-3 sm:mt-2 lg:ml-10 md:ml-0 sm:ml-0"
+//           >
+//             <div className="w-8 h-8 flex items-center justify-center rounded-full bg-red-500">
+//               <i className="fas fa-search text-white"></i>
+//             </div>
+//           </Link>
+//         </div>
+//       </div>
+
+
+      
+//       {/* Horizontal scrolling */}
+
+//       <div className="w-full hidden  p-2 lg:flex justify-center pt-1 mx-auto">
+//         {/* <span className="text-white text-sm pt-1 font-semibold flex items-center">
+//     Popular Search
+//   </span> */}
+
+//         <div className="flex animate-scroll pt-2  flex-nowrap overflow-hidden">
+//           {primeLocations.map((location, index) => (
+//             <div key={index} className="flex-shrink-0 px-1">
+//               {" "}
+//               {/* Reduce px spacing */}
+//               <Link to={location.to} target="_top" rel="noopener noreferrer">
+//                 <span className="bg-white  rounded-full text-red-600 font-semibold text-[12px] px-2 py-1 hover:text-red-500 hover:underline-offset-8 cursor-pointer text-center whitespace-nowrap">
+//                   {location.name}
+//                 </span>
+//               </Link>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+
+//     </>
+//   );
+// };
+
+// export default Search;
+
+
+import { FaArrowRight } from "react-icons/fa";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 const Search = ({ data1 }) => {
@@ -47,43 +188,16 @@ const Search = ({ data1 }) => {
     }
   };
 
-  const placeholders = [
-    'Search "Villas"',
-    'Search "3 BHK Ready To Move Flat For Sale In Gurgaon"',
-    'Search "Best Properties"',
-    'Search "Delhi NCR"',
-    'Search "3 BHK Sale For Goa"',
-    'Search "Commercial Space For Sale In Gurgaon"',
-    // Add more placeholders as needed
-  ];
-
-  const [currentPlaceholder, setCurrentPlaceholder] = useState(placeholders[0]);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentPlaceholder((prev) => {
-        const currentIndex = placeholders.indexOf(prev);
-        const nextIndex = (currentIndex + 1) % placeholders.length;
-        return placeholders[nextIndex];
-      });
-    }, 3000);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
   return (
     <>
-      <div
-        className="w-70 shadow-xl bg-white border-none lg:h-14 md:h-10 sm:h-8 px-2 lg:px-4 md:px-3 sm:px-2"
-        style={{ marginTop: window.innerWidth < 640 ? "200px" : "0" }}
-      >
+      <div className="w-70 bg-white border-white border-none lg:h-14 md:h-10 sm:h-8 rounded-lg lg:rounded-2xl md:rounded-xl sm:rounded-lg px-2 lg:px-4 md:px-3  sm:px-2">
         <div className="flex items-center xl:h-14 lg:h-14 md:h-10 sm:h-8">
-          <div className="w-60 mt-1 lg:mt-3 md:mt-3 sm:mt-12 ml-2 lg:ml-8 md:ml-6 sm:ml-4 lg:w-[620px] md:w-full sm:w-70">
+          <div className="w-60 mt-1 lg:mt-3 md:mt-3 sm:mt-2 ml-2 lg:ml-8 md:ml-6 sm:ml-4 lg:w-[820px] md:w-full sm:w-70 outline-none">
             <input
               className="outline-none w-full"
               type="text"
               name="query"
-              placeholder={currentPlaceholder}
+              placeholder="Enter Your Query"
               value={formData.query}
               onChange={handleInputChange}
               onKeyPress={handleKeyPress}
@@ -98,7 +212,7 @@ const Search = ({ data1 }) => {
               state: formData,
             }}
             id="searchButton"
-            className="ml-2 my-1 mt-1 lg:mt-3 md:mt-3 sm:mt-2 lg:ml-10 md:ml-0 sm:ml-0"
+            className="ml-2 my-1 mt-1 lg:mt-3 md:mt-3 sm:mt-2 lg:ml-0 md:ml-0 sm:ml-0"
           >
             <div className="w-8 h-8 flex items-center justify-center rounded-full bg-red-500">
               <i className="fas fa-search text-white"></i>
@@ -107,22 +221,18 @@ const Search = ({ data1 }) => {
         </div>
       </div>
 
-
-      
       {/* Horizontal scrolling */}
 
-      <div className="w-full hidden  p-2 lg:flex justify-center pt-1 mx-auto">
-        {/* <span className="text-white text-sm pt-1 font-semibold flex items-center">
-    Popular Search
-  </span> */}
+      <div className="w-full hidden lg:flex justify-center pt-1  mx-auto">
+        <span className="text-white  text-sm pt-1 font-semibold flex items-center">
+          Popular Search
+        </span>
 
-        <div className="flex animate-scroll pt-2  flex-nowrap overflow-hidden">
+        <div className="flex animate-scroll pt-1">
           {primeLocations.map((location, index) => (
-            <div key={index} className="flex-shrink-0 px-1">
-              {" "}
-              {/* Reduce px spacing */}
+            <div key={index} className="w-auto flex-shrink-0 px-[2px]">
               <Link to={location.to} target="_top" rel="noopener noreferrer">
-                <span className="bg-white  rounded-full text-red-600 font-semibold text-[12px] px-2 py-1 hover:text-red-500 hover:underline-offset-8 cursor-pointer text-center whitespace-nowrap">
+                <span className="bg-white rounded-full  text-black font-semibold text-[12px] px-2 py-1 hover:text-red-500 hover:underline-offset-8 cursor-pointer text-center">
                   {location.name}
                 </span>
               </Link>
@@ -130,7 +240,6 @@ const Search = ({ data1 }) => {
           ))}
         </div>
       </div>
-
     </>
   );
 };
