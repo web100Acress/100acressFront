f9feff45d6bf3087192e7c76259c7fc45adac128
web100Acress <web.100acress@gmail.com>
2025-01-07 12:02:47 +0530

    search button animation & project page bug fix

--- src/aadharhomes/Search.js
+++ src/aadharhomes/Search.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { SearchIcon } from "../Assets/icons";
 import AOS from 'aos';
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import 'aos/dist/aos.css';
 import { Wrap } from "@chakra-ui/react";
+import { useMediaQuery } from "@chakra-ui/react";
+
 
 const Search = ({ data1 }) => {
   const [formData, setFormData] = useState({
@@ -12,7 +14,7 @@ const Search = ({ data1 }) => {
     query: "",
     collectionName: data1,
   });
-
+  const [isSmallerThan425] =  useMediaQuery("(max-width: 425px)");
 
   useEffect(() => {
     setFormData((prevState) => ({
@@ -101,11 +103,31 @@ const Search = ({ data1 }) => {
           }}
           id="searchButton"
         >
-          <div className="px-2.5 md:px-5 py-2.5 bg-[#C13B44] text-white rounded-full flex items-center justify-center gap-1 ">
-            <SearchIcon /> {" "}<span className="hidden sm:block">
-              Search
+          { !isSmallerThan425 &&
+
+            <div className="px-2.5 md:px-5 py-2.5 text-white rounded-full flex items-center justify-center gap-1 ">
+          <StyledLink href='#'>
+            <Text className="hidden sm:block text-xs">
+              <span>
+              <SearchIcon style={{ width: '10px', height: '10px' }} />{" "}
               </span>
+              Search
+              </Text>
+              <Liquid className="liquid"/>
+              </StyledLink>
           </div>
+          }
+          { isSmallerThan425 &&
+
+          <div className="px-2.5 mr-2 md:px-5 py-2.5 text-white rounded-full flex items-center justify-center gap-1 ">
+          <StyledLink href='#'>
+            <Text className="text-xs">
+              Search
+              </Text>
+              <Liquid className="liquid"/>
+              </StyledLink>
+          </div>
+          }
         </Link>
       </div>
     </div>
@@ -117,4 +139,79 @@ export default Search;
 
 const Wrapper = styled.section`
   
+`;
+
+const animate = keyframes`
+  0% {
+    transform: translate(-25%, -75%) rotate(0);
+  }
+  100% {
+    transform: translate(-25%, -75%) rotate(360deg);
+  }
+`;
+
+const StyledLink = styled.a`
+  font:  2px !important ;
+  color: #C13B44;
+  text-decoration: none;
+  padding: 6px 12px;
+  position: relative;
+  overflow: hidden;
+  border-radius: 20px;
+  transition: 0.2s;
+  transform: scale(1.5);
+
+  &:hover {
+    box-shadow: 0 0 5px #C13B44, inset 0 0 5px #C13B44;
+    transition-delay: 0.2s;
+  }
+
+  &:hover .liquid {
+    top: -40px;
+  }
+`;
+
+const Text = styled.span`
+  size : 3px !important;
+  position: relative;
+  z-index: 0;
+  color: #fff;
+`;
+
+const Liquid = styled.div`
+  position: absolute;
+  top: -12px;
+  left: 0;
+  width: 100%;
+  height: 100px;
+  background: #C13B44;
+  box-shadow: inset 0 0 50px rgba(0, 0, 0, 0.7);
+  z-index: -1;
+  transition: 0.6s;
+
+  &::after,
+  &::before {
+    position: absolute;
+    content: "";
+    width: 200%;
+    height: 200%;
+    top: 0;
+    left: 0;
+    transform: translate(-25%, -75%);
+  }
+
+  &::after {
+    border-radius: 45%;
+    background: #C13B44;
+    box-shadow: 0 0 10px 5px #C13B44, inset 0 0 5px #C13B44;
+    animation: ${animate} 5s linear infinite;
+    opacity: 0.8;
+  }
+
+  &::before {
+    border-radius: 40%;
+    box-shadow: 0 0 10px rgba(26, 26, 26, 0.5), inset 0 0 5px rgba(26, 26, 26, 0.5);
+    background: rgba(26, 26, 26, 0.5);
+    animation: ${animate} 7s linear infinite;
+  }
 `;
