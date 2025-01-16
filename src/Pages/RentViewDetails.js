24e7c740a10753f1e965e7b32fc5a9171765a9b0
Anshuman Tiwari <anshuman.100acress@gmail.com>
2025-01-16 14:15:40 +0530

    Dynamic Rendering of the Page is done

--- src/Pages/RentViewDetails.js
+++ src/Pages/RentViewDetails.js
@@ -5,9 +5,10 @@ import axios from "axios";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import {SyncLoader} from "react-spinners";
 import { Link, useParams } from "react-router-dom";
 import { DataContext } from "../MyContext";
-import {LocationIcon, HeartIcon, ShareIcon,PhoneIcon, CarParkingIcon, ForwardIcon, BackwardIcon, LcoationBiggerIcon, ArrowIcon} from "../Assets/icons";
+import {LocationIcon, HeartIcon, ShareIcon,PhoneIcon, CarParkingIcon, ForwardIcon, BackwardIcon, LcoationBiggerIcon, ArrowIcon, RupeeIcon} from "../Assets/icons";
 import styled from "styled-components";
 
 
@@ -47,36 +48,39 @@ const RentViewDetails = () => {
     }
   }
 `;
-  const [rentViewDetails, setRentViewDetails] = useState({
-    descripation: "",
-  });
+  const [rentViewDetails, setRentViewDetails] = useState();
   const [showForm, setShowForm] = useState(false);
   const [showContact, setShowContact] = useState(false);
   const [getContact, setGetContact] = useState("");
   const [buyData, setBuyData] = useState("");
+  const [loading, setLoading] = useState(false);
 
 
-  const { frontImage, otherImage, amenities } = rentViewDetails;
+  // const { frontImage, otherImage, amenities } = rentViewDetails;
 
   const { id } = useParams();
   console.log(id);
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const res = await axios.get(
           `https://api.100acress.com/property/view/${id}`
         );
         if (res.data.data) {
           setRentViewDetails(res.data.data);
+          
         } else {
           setRentViewDetails(res.data.postData.postProperty[0]);
+          
         }
         console.log(
           res.data.postData.postProperty[0],
-          "res.data.postData.postProperty[0]"
+          "Property Information"
         );
       } catch (error) {
         console.error("Error fetching data:", error);
+        
       }
     };
     fetchData();
@@ -247,7 +251,9 @@ const RentViewDetails = () => {
     try {
       const res = await axios.get(
         "https://api.100acress.com/property/buy/ViewAll"
+
       );
+      console.log(res.data.collectdata, "All Buyable Property Information");
       setBuyData(res.data.collectdata);
     } catch (error) {
       console.error("Error fetching Data", error);
@@ -259,88 +265,89 @@ const RentViewDetails = () => {
   }, []);
 
   const { trendingProject } = useContext(DataContext);
+
+  console.log(trendingProject, "trending project");
  
   return (
     <div style={{ overflowX: "hidden" }}>
       <Wrapper>
-
-      
-      <div className="block w-11/12 mx-auto mt-20 mb-8">
-
-      <div className="grid grid-cols-2 border">
-        <div className="border mt-auto">
-          <div>
-            <h3>4S The Aurrum</h3>
-            <h6>4S Developers</h6>
-            <p className="flex"><LocationIcon color="#C13B44"/><span>Sector 59, Golf Course Extn Road, Gurugram</span></p>
-          </div>
-        </div>
-        <div className="flex justify-end border self-end">
-            <div>
-              <h5 className="text-end">₹2.3Cr -5.1Cr</h5>
-              <div className="flex gap-4">
-                <HeartIcon />
-                <ShareIcon />
-                <div className="bg-primaryRed text-white px-3 py-2 rounded"><PhoneIcon/>7760540078</div>
+        {rentViewDetails&&(
+            <div className="block w-11/12 mx-auto mt-20 mb-8">
+            
+            <div className="grid grid-cols-2 gap-4">
+              <div className="mt-auto">
+                <div>
+                  <h3 className="capitalize">{rentViewDetails.propertyName}</h3>
+                  <h6 className="capitalize"><span className="font-medium mr-2">{rentViewDetails.propertyType}</span><span>{rentViewDetails.area}</span></h6>
+                  <p className="flex"><LocationIcon color="#C13B44"/><span className="ml-2 capitalize">{rentViewDetails.address}, {rentViewDetails.city}, {rentViewDetails.state}</span></p>
+                </div>
+              </div>
+              <div className="flex justify-end self-end">
+                  <div>
+                    <h5 className="text-end text-primaryRed font-semibold">Rent <RupeeIcon/>{rentViewDetails.price}</h5>
+                    <div className="flex gap-4">
+                      <HeartIcon />
+                      <ShareIcon />
+                      <div className="bg-primaryRed text-white px-3 py-2 rounded"><PhoneIcon/>{rentViewDetails.number}</div>
+                    </div>
+                  </div>
               </div>
             </div>
-        </div>
-      </div>
 
-      <img className="rounded-lg my-2" src="https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/project/q49dy9od7ygss5onqayf" alt="Project name"/>
-      
-      <div className="grid grid-cols-3 gap-4">
 
-        <div className="border col-span-2">
-          <div>
-            <h3>About Property</h3>
-            <p>Gurugram's beating heart, the New Golf Course Road Extension, has blossomed into a thriving hub. This growing community offers a vibrant mix of attractions & amenities creating a harmonius balance between urban energy & neighborly charm. Discover a place where friendly neighborhoods seamlessly connect with the city's pulse, offering a lifestyle as diverse as it is dynamic.</p>
-          </div>
-          <div>
-            <h3>Property Highlights</h3>
-              <ul className="list-disc">
-                <li>3 Side Open Residences</li>
-                <li>Triple Height Lobby</li>
-                <li>Spacious Sun Decks</li>
-                <li>2 Private Elevators in Every Tower</li>
-                <li>Ample Car Parking</li>
-              </ul>
-          </div>
-          <div className="">
-            <h3>Amenities</h3>
-            <div className="flex flex-col justify-center items-center w-1/3 bg-white p-4 shadow rounded-lg" >
-                <CarParkingIcon/>
-                <span>Car Parking</span>
-            </div>
-          </div>
-            <div>
-              <div className="my-4 ">
-              <h5 className="text-4xl">Other Images</h5>
-                <ImageGallery images={Data}/>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+
+              <div className="col-span-2 h-fit">
+                <img className="rounded-lg my-2 object-cover w-full h-[80vh]" src={rentViewDetails.frontImage.url} alt="Project name"/>
+                <div className="my-4">
+                  <h3>About Property</h3>
+                  <p>{rentViewDetails.descripation}</p>
+                </div>
+                <div className="my-4">
+                  <h3>Property Highlights</h3>
+                    <ul className="list-disc capitalize">
+                      <li className={`${rentViewDetails.area ? " ":"hidden"}`}>{rentViewDetails.area}</li>
+                      <li className={`${rentViewDetails.furnishing ? " ":"hidden"}`}>{rentViewDetails.furnishing}</li>
+                      <li className={`${rentViewDetails.landMark ? " ":"hidden"}`}>{rentViewDetails.landMark}</li>
+                      <li className={`${rentViewDetails.type ? " ":"hidden"}`}>{rentViewDetails.type}</li>
+                    </ul>
+                </div>
+                <div className="my-4">
+                  <h3>Amenities</h3>
+                  <div className="flex flex-col justify-center items-center w-1/3 bg-white my-4 p-4 shadow rounded-lg" >
+                      <CarParkingIcon/>
+                      <span>Car Parking</span>
+                  </div>
+                </div>
+                  <div>
+                    <div className="my-4 ">
+                    <h5 className="text-4xl">Other Images</h5>
+                      <ImageGallery images={rentViewDetails.otherImage}/>
+                    </div>
+                    <div className="my-4 relative">
+                      <h5 className="text-4xl">Similar Properties</h5>
+                      <Carousel trendingProject={trendingProject}/>
+                    </div>
+                </div>
               </div>
-              <div className="my-4 relative">
-                <h5 className="text-4xl">Similar Properties</h5>
-                <Carousel images={Data}/>
+              <div className="col-span-1 my-2">
+                  <div className="shadow-md p-4 rounded-lg mb-4 border-[0.2px]">
+                    <h3>Know more about property</h3>
+                    <input type="text" name="fullName" placeholder="Full Name" className="w-full border p-2 my-2"/>
+                    <input type="number" name="mobileNumber" placeholder="Mobile Number" className="w-full border p-2 my-2"/>
+                    <input type="email" name="email" placeholder="Email address" className="w-full border p-2 my-2"/>
+                    <p className="text-primaryRed">Fill out form only one - time. After get the contact number</p>
+                    <button className="block w-full px-5 py-2 bg-primaryRed text-white rounded-md">Get Details</button>
+                  </div>
+                  <div className="shadow-md p-4 rounded-lg my-4 border-[0.2px]">
+                      <h5>Post your Property for <span className="text-primaryRed">FREE!</span></h5>
+                      <button className="block w-full px-5 py-2 bg-primaryRed text-white rounded-md">List Properties <span className="bg-white text-primaryRed p-1 mx-1 rounded">FREE</span></button>
+                  </div>
               </div>
-          </div>
-        </div>
-        <div className="col-span-1 ">
-            <div className="shadow-md p-4 rounded-lg mb-4 border-[0.2px]">
-              <h3>Know more about property</h3>
-              <input type="text" name="fullName" placeholder="Full Name" className="w-full border p-2 my-2"/>
-              <input type="number" name="mobileNumber" placeholder="Mobile Number" className="w-full border p-2 my-2"/>
-              <input type="email" name="email" placeholder="Email address" className="w-full border p-2 my-2"/>
-              <p>Fill out form only one - time. After get the contact number</p>
-              <button className="block w-full px-5 py-2 bg-primaryRed text-white rounded-md">Get Details</button>
-            </div>
-            <div className="shadow-md p-4 rounded-lg my-4 border-[0.2px]">
-                <h5>Post your Property for <span>FREE!</span></h5>
-                <button className="block w-full px-5 py-2 bg-primaryRed text-white rounded-md">List Properties <span>FREE</span></button>
-            </div>
-        </div>
 
-      </div>
-      </div>
+            </div>
+            </div>
+            )}
       </Wrapper>
       <Footer />
     </div>
@@ -406,19 +413,19 @@ const ImageGallery  = ({images})=>{
         onMouseLeave={handleMouseLeave}
       >
         {imagesToRender.map((image) => (
-          <img key={image.id} src={image.imgSrc} alt={`Image ${image.id}`} className="h-32 w-48 rounded-md"
-          onClick={() => handleOpenModal(image.imgSrc)}
+          <img key={image.public_id} src={image.url} alt={`Image ${image.public_id}`} className="h-36 w-52 rounded-md object-cover"
+          onClick={() => handleOpenModal(image.url)}
           onDragStart={(e) => e.preventDefault()}
           />
         ))}
               {images.length > 4 && !showAll && (
                 <div className="relative">
                   <img
-                    src={images[3].imgSrc}
+                    src={images[3].url}
                     alt={showAll ? 'Show Less' : 'Show More'} 
-                    className="flex h-32 w-48 rounded-md basis-1/4 opacity-75 hover:opacity-100 cursor-pointer"
+                    className="flex h-36 w-52 rounded-md basis-1/4 opacity-75 hover:opacity-100 cursor-pointer"
                     />
-                  <div className="absolute top-0 left-0 h-32 w-48 rounded bg-black/70 text-white flex items-center justify-center text-center"
+                  <div className="absolute top-0 left-0 h-36 w-52 rounded bg-black/70 text-white flex items-center justify-center text-center"
                   onClick={toggleShowAll}
                   >
                     +{images.length - 3} photos
@@ -451,7 +458,7 @@ const ImageGallery  = ({images})=>{
 }
 
 // Carousel
-const Carousel = ({images})=>{
+const Carousel = ({trendingProject})=>{
 
   const sliderRef = useRef();
 
@@ -477,38 +484,38 @@ const Carousel = ({images})=>{
     <>
       <button
         onClick={() => sliderRef.current.slickPrev()}
-        className="absolute top-0 lg:top-0 sm:top-0 right-10 transform -translate-y-0 bg-white text-gray-500 p-2 rounded-full shadow-xl z-10"
+        className="mr-2 absolute top-0 lg:top-0 sm:top-0 right-10 transform -translate-y-1 bg-white text-gray-700 p-2 rounded-full shadow-xl z-10"
       >
         <BackwardIcon />
       </button>
       <button
         onClick={() => sliderRef.current.slickNext()}
-        className="absolute top-0 lg:top-0 sm:top-0 right-0 transform -translate-y-0 text-gray-700 bg-white p-2 rounded-full shadow-xl z-10"
+        className="ml-2 absolute top-0 lg:top-0 sm:top-0 right-0 transform -translate-y-0 text-gray-700 bg-white p-2 rounded-full shadow-xl z-10"
       >
-        <ForwardIcon />
+        <ForwardIcon/>
       </button>
       <div className="">
         <Slider {...setting} ref={sliderRef} >
-          {images.map((image) => (
+          {trendingProject.map((project) => (
           <section className="" >
             <div className="w-full">
                 {/* const pUrl = item.project_url; */}
                     <Link  target="_top">
                       <article
-                        key={image.id}
-                        className="mx-2 transition overflow-hidden rounded-md border text-gray-700 shadow-md duration-500 ease-in-out hover:shadow-xl"
+                        key={project._id}
+                        className="mx-2 transition overflow-hidden rounded-md  text-gray-700 shadow-md duration-500 ease-in-out hover:shadow-xl"
                         onDrag={(e)=>{e.preventDefault()}}
                       >
                         <div className="p-3 relative overflow-hidden">
                           <img
-                            src={image.imgSrc}
+                            src={project.frontImage.url}
                             alt="property In Gurugram"
                             className="w-full h-[200px] object-cover rounded-lg transition-transform duration-500 ease-in-out hover:scale-110"
                           />
                         </div>
                         <div className="p-2 pt-0">
                           <span className="text-[15px] font-semibold text-black-600 hover:text-red-600 duration-500 ease-in-out">
-                            {"4S Aradhya Homes"}
+                            {project.projectName}
                           </span>
 
                           <ul className="m-0 p-0 flex text-white-600 justify-between px-0 pb-0">
@@ -520,10 +527,10 @@ const Carousel = ({images})=>{
                               {/* Text */}
                               <div className="text-sm font-thin truncate w-64 md:w-64 lg:w-32 xl:w-48">
                                 <span className="text-sm text-white-600 hover:text-red-600 duration-500 ease-in-out block truncate">
-                                  {"Gurugram, Haryana"}
+                                  {project.projectAddress}
                                 </span>
                                 <span className="text-xs text-[#656565] block truncate hover:overflow-visible hover:white-space-normal hover:bg-white">
-                                  {"Gurugram, Haryana"}
+                                  {project.city},{project.state}
                                 </span>
                               </div>
                             </li>
