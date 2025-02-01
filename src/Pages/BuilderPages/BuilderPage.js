ea36802b12b93363d63067dd4b1e2dbda52d7fd1
web100Acress <web.100acress@gmail.com>
2025-02-01 13:04:57 +0530

    smartworld page fix

--- src/Pages/BuilderPages/BuilderPage.js
+++ src/Pages/BuilderPages/BuilderPage.js
@@ -28,13 +28,13 @@ const BuilderPage = () => {
     'elan-group': Elan,
     'bptp-limited':BPTP,
     'adani-realty':Adani,
-    'smartWorld-developers':SmartWorld, 
+    'smartworld-developers':SmartWorld, 
     'trevoc-group':Trevoc,
     'indiabulls-real-estate':IndiaBulls 
 
   };
 
-  const builderProjects = buildersData[builderName] || []; // Handle undefined builder gracefully
+  const builderProjects = buildersData[builderName] || []; 
 
   const handleShare = (project) => {
     if (navigator.share) {
@@ -53,9 +53,9 @@ const BuilderPage = () => {
 
   const formatBuilderName = (name) => {
     return name
-      .split('-') // Split the name into an array using hyphens
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize each word
-      .join(' '); // Join the words with spaces
+      .split('-') 
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1)) 
+      .join(' '); 
   };
   
 
@@ -76,87 +76,6 @@ const BuilderPage = () => {
         />
       </Helmet>
 
-      {/* <section className="flex pt-2 flex-col items-center mt-12">
-        <h1 className="mb-3 p-3 text-center text-2xl sm:text-xl md:text-2xl lg:text-3xl text-red-600 font-bold">
-          {builderName} Projects in Gurugram
-        </h1>
-
-        <div className="grid max-w-md grid-cols-1 px-8 sm:max-w-lg md:max-w-screen-xl md:grid-cols-2 md:px-4 lg:grid-cols-4 sm:gap-4 lg:gap-4 w-full">
-          {builderProjects.map((item, index) => {
-            const pUrl = item.project_url;
-            return (
-              <span key={index}>
-                <article className="mb-2 overflow-hidden rounded-md border text-gray-700 shadow-md duration-500 ease-in-out hover:shadow-xl">
-                  <div className="relative flex p-3">
-                    <Link to={`/${pUrl}/`} target="_top">
-                      <img
-                        src={item.frontImage.url}
-                        alt={item.projectName}
-                        className="w-full h-48 object-fit rounded-lg transition-transform duration-500 ease-in-out hover:scale-110"
-                      />
-                    </Link>
-                    <div
-                      className="absolute top-5 right-5"
-                      onClick={() => handleShare(item)}
-                    >
-                      <ShareFrameIcon />
-                    </div>
-                  </div>
-                  <div className="pt-0 p-3">
-                    <div className="pb-2">
-                      <span className="text-[15px] font-semibold hover:text-red-600 duration-500 ease-in-out">
-                        {item.projectName}
-                      </span>
-                      <br />
-                      <span className="text-sm text-gray-400 hover:text-red-600 duration-500 ease-in-out">
-                        {item.city}, {item.state}
-                      </span>
-                    </div>
-
-                    <ul className="box-border flex list-none items-center border-b border-solid border-gray-200 px-0 py-2">
-                      <li className="mr-4 flex items-center text-left">
-                        <p className="m-0 text-sm font-medium">
-                          <PropertyIcon /> {item.type}
-                        </p>
-                        <span className="text-[10px] text-gray-600 block truncate text-sm hover:overflow-visible hover:white-space-normal hover:bg-white">
-                          <LocationRedIcon /> {item.projectAddress}
-                        </span>
-                      </li>
-                    </ul>
-
-                    <ul className="m-0 flex list-none items-center justify-between px-0 pb-0">
-                      <li className="text-left">
-                        <span className="text-sm font-extrabold text-red-600">
-                          <span className="text-xl">
-                            <RupeeIcon />
-                          </span>
-                          {item.minPrice < 1 ? (
-                            <>{item.minPrice * 100} L</>
-                          ) : (
-                            <>{item.minPrice}</>
-                          )}
-                          {" - "}
-                          {item.maxPrice} Cr
-                        </span>
-                      </li>
-                      <Link to={`/${pUrl}/`} target="_top">
-                        <li className="text-left">
-                          <button
-                            type="button"
-                            className="text-white bg-gradient-to-r from-[#C13B44] via-red-500 to-[#C13B44] hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-xs px-4 py-1.5 text-center"
-                          >
-                            View Details
-                          </button>
-                        </li>
-                      </Link>
-                    </ul>
-                  </div>
-                </article>
-              </span>
-            );
-          })}
-        </div>
-      </section> */}
       <section className="flex pt-2 flex-col items-center mt-12">
               <h1 className="mb-3 p-3 text-center text-2xl sm:text-xl md:text-2xl lg:text-3xl text-red-600 font-bold">
               {formattedBuilderName} Projects in Gurugram
