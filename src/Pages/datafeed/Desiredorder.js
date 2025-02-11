56e69a61f34c598f387db136a6d1f1e947d43564
web100Acress <web.100acress@gmail.com>
2025-02-11 18:24:31 +0530

    feeding change

--- src/Pages/datafeed/Desiredorder.js
+++ src/Pages/datafeed/Desiredorder.js
@@ -13,4 +13,53 @@ export const Trending_Desired_Order =[
 
 export const Affordable_Desired_Order = [
     "TLC The First Acre",
-]
+];
+
+export const imageSrc = [
+    {
+      image:"https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/t3.webp", link:"/developers/signature-global/"
+    },
+    {
+      image:"https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/t1.webp", link:"/indiabulls-estate-club/"
+    },
+    {
+      image:"https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/t2.webp", link:"/trevoc-56-gurgaon/"
+    },
+    {
+      image:"https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/t4.webp", link:"/puri-diplomatic-residences/"
+    },
+    {
+      image:"https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/t5.webp", link:"/developers/m3m-india/"
+    },
+    {
+      image:"https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/t6.webp", link:"/oxirich-chintamani/"
+    },
+    {
+      image:"https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/t7.webp", link:"/elan-the-emperor/"
+    },
+
+  ];
+
+export const phoneSrc = [
+  {
+    image:"https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/phone.webp", link:"/developers/signature-global/"
+  },
+  {
+    image:"https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/phone2.webp", link:"/trevoc-56-gurgaon/"
+  },
+  {
+    image:"https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/phone3.webp", link:"/indiabulls-estate-club/"
+  },
+  {
+    image:"https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/phone4.webp", link:"/puri-diplomatic-residences/"
+  },
+  {
+    image:"https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/phone5.webp", link:"/developers/m3m-india/"
+  },
+  {
+    image:"https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/phone6.webp", link:"/oxirich-chintamani/"
+  },
+  {
+    image:"https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/phone7.webp", link:"/elan-the-emperor/"
+  },
+];
