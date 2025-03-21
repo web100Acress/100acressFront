51876de8bb3d3624bbd56ccb5bd7af26b8b2918e
Parwinder <parwinderacress@gmail.com>
2025-03-21 10:49:39 +0530

    make changes in projects section

--- src/Pages/NewLaunch.jsx
+++ src/Pages/NewLaunch.jsx
@@ -11,7 +11,21 @@ const NewLaunch = () => {
   return (
     <div>
       {/* <NewLaunchSearch /> */}
-      <div className="mt-16">
+      <div className="mt-auto">
+      <div className="max-w-screen pt-2 sm:pt-2 md:pt-2" target="_top">
+        <h1 className=" p-1 text-center text-2xl mt-16 sm:text-xl md:text-2xl lg:text-3xl text-red-600 font-bold ">
+          New Launch Projects in Gurgaon
+        </h1> 
+
+        <h2 className="text-sm text-justify mb-4  sm:text-xl md:text-xl lg:text-sm font-normal lg:mx-20 md:mx-10 mx-5 sm:mx-4">
+        New Launch Properties in Gurgaon include commercial and residential
+          projects that will meet various requirements. These developments are
+          equipped with modern amenities, great places close to business areas,
+          as well as extensive green spaces. They're designed to meet the
+          ever-changing demands of urban dwellers who want peace, convenience,
+          and a vibrant lifestyle.
+        </h2>
+      </div>
         <ProjectSearching setFilteredProjectsParent={setFilteredProjectsParent}/>
       </div>
       <Helmet>
@@ -23,7 +37,6 @@ const NewLaunch = () => {
           content="Real Estate Properties in New Panipat- Get Details for Residential &amp; Commercial Properties"
         />
       </Helmet>
-
         {filteredProjectsParent.length === 0 &&
         <CommonProject
         data={newLaunch}
--- src/Pages/UnderConstruction.jsx
+++ src/Pages/UnderConstruction.jsx
@@ -122,6 +122,20 @@ const UnderConstruction = () => {
 
   return (
     <div>
+      <div className="max-w-screen pt-2 sm:pt-2 md:pt-2" target="_top">
+        <h1 className=" p-1 text-center text-2xl mt-16 sm:text-xl md:text-2xl lg:text-3xl text-red-600 font-bold ">
+        Under Construction Projects in Gurgaon
+        </h1> 
+
+        <h2 className="text-sm text-justify mb-4  sm:text-xl md:text-xl lg:text-sm font-normal lg:mx-20 md:mx-10 mx-5 sm:mx-4">
+        Under Construction Properties in Gurgaon include commercial and residential
+          projects that will meet various requirements. These developments are
+          equipped with modern amenities, great places close to business areas,
+          as well as extensive green spaces. They're designed to meet the
+          ever-changing demands of urban dwellers who want peace, convenience,
+          and a vibrant lifestyle.
+        </h2>
+      </div>
       <Helmet>
         <title>
           Property in UnderConstruction - Flats, Villas, House in gurugram.
@@ -131,7 +145,7 @@ const UnderConstruction = () => {
           content="Real Estate Properties in New Panipat- Get Details for Residential &amp; Commercial Properties"
         />
       </Helmet>
-      <div className="mt-16">
+      <div className="mt-auto">
         <ProjectSearching setFilteredProjectsParent={setFilteredProjectsParent}/>
       </div>
         {filteredProjectsParent.length === 0 &&
