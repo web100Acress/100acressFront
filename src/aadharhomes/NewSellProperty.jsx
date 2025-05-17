e4a2873a5534599a6e0e5a68be65442ad37c7a11
Parwinder <parwinderacress@gmail.com>
2025-05-17 18:23:02 +0530

    increased the no. of bedrooms and bathrooms in property section

--- src/aadharhomes/NewSellProperty.jsx
+++ src/aadharhomes/NewSellProperty.jsx
@@ -472,6 +472,9 @@ const NewSellProperty = () => {
               <option value="2">2</option>
               <option value="3">3</option>
               <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+
             </select>
             <select
               name="bathrooms"
@@ -484,6 +487,8 @@ const NewSellProperty = () => {
               <option value="2">2</option>
               <option value="3">3</option>
               <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
             </select>
           </>
         )}
