dbe9022885ceecde247cb63249e7dc7af25a60b8
ashutosh04k <kumarasutosh2014@gmail.com>
2025-06-17 16:06:56 +0530

    adeed banner

--- src/Pages/datafeed/Desiredorder.jsx
+++ src/Pages/datafeed/Desiredorder.jsx
@@ -1,25 +1,25 @@
 export const Luxury_Desired_Order = [
-    "Elan The Emperor",
-    "Experion The Trillion",
-    "Birla Arika",
-    "DLF Privana North",
+  "Elan The Emperor",
+  "Experion The Trillion",
+  "Birla Arika",
+  "DLF Privana North",
 ];
 
-export const Trending_Desired_Order =[
-    "Indiabulls Estate Club",
-    "Signature Global Twin Tower DXP",
-    "Tarc Ishva"
+export const Trending_Desired_Order = [
+  "Indiabulls Estate Club",
+  "Signature Global Twin Tower DXP",
+  "Tarc Ishva"
 ];
 
 export const Affordable_Desired_Order = [
-    "TLC The First Acre",
+  "TLC The First Acre",
 ];
 
 export const SCO_DESIRED_ORDER = [
-    "Reach The Bazaria",
-    "BPTP The Oval",
-    "Aarize South Drive",
-    "Adani Downtown Avenue"
+  "Reach The Bazaria",
+  "BPTP The Oval",
+  "Aarize South Drive",
+  "Adani Downtown Avenue"
 ];
 
 export const COMMERCIAL_DESIRED_ORDER = [
@@ -31,70 +31,70 @@ export const COMMERCIAL_DESIRED_ORDER = [
 
 export const imageSrc = [
   {
-    image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/west.webp", link:"/whiteland-westin-residences/"
+    image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/west.webp", link: "/whiteland-westin-residences/"
   },
-    {
-      image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/titanium-lg.webp", link:"/signature-global-titanium/"
-    },
-    {
-      image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/experionlaptop.jpeg", link:"/experion-the-trillion/"
-    },
-    {
-      image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/trevoc-laptop.jpeg", link:"/trevoc-56-gurgaon/"
-    },
-    {
-      image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/windchant-laptop.jpeg",link:"/experion-nova-at-windchants/"
-    },
-    {
-      image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/bazaria-laptop.jpeg",link:"/reach-the-bazaria/"
-    },
-    {
-      image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/sky-p-laptop.jpeg",link:"/trinity-sector-88b-gurgaon/"
-    },
-    {
-      image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/bptplaptop.jpeg", link:"/bptp-verti-greens/"
-    },
-  ];
+  {
+    image: "https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/spr-web.jpg", link: "/signature-global-cloverdale-spr/"
+  },
+  {
+    image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/experionlaptop.jpeg", link: "/experion-the-trillion/"
+  },
+  {
+    image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/trevoc-laptop.jpeg", link: "/trevoc-56-gurgaon/"
+  },
+  {
+    image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/windchant-laptop.jpeg", link: "/experion-nova-at-windchants/"
+  },
+  {
+    image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/bazaria-laptop.jpeg", link: "/reach-the-bazaria/"
+  },
+  {
+    image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/sky-p-laptop.jpeg", link: "/trinity-sector-88b-gurgaon/"
+  },
+  {
+    image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/bptplaptop.jpeg", link: "/bptp-verti-greens/"
+  },
+];
 
 export const phoneSrc = [
   {
-      image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/westmob.webp", link:"/whiteland-westin-residences/"
+    image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/westmob.webp", link: "/whiteland-westin-residences/"
   },
   {
-    image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/titanium-sm.webp", link:"/signature-global-titanium/"
+    image: "https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/banner/spr-mob.jpg", link: "/signature-global-cloverdale-spr/"
   },
   {
-    image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/trevoc-phone.jpeg", link:"/trevoc-56-gurgaon/"
+    image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/trevoc-phone.jpeg", link: "/trevoc-56-gurgaon/"
   },
   {
-    image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/experionphone.jpeg", link:"/experion-the-trillion/"
+    image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/experionphone.jpeg", link: "/experion-the-trillion/"
   },
   {
-    image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/windchant-phone.jpeg", link:"/experion-nova-at-windchants/"
+    image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/windchant-phone.jpeg", link: "/experion-nova-at-windchants/"
   },
   {
-    image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/bazaria-phone.jpeg", link:"/reach-the-bazaria/"
+    image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/bazaria-phone.jpeg", link: "/reach-the-bazaria/"
   },
   {
-    image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/sky-p-phone.jpeg", link:"/trinity-sector-88b-gurgaon/"
+    image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/sky-p-phone.jpeg", link: "/trinity-sector-88b-gurgaon/"
   },
   {
-    image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/bptpphone.jpeg", link:"/bptp-verti-greens/"
+    image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/banner/bptpphone.jpeg", link: "/bptp-verti-greens/"
   },
 ];
 
 export const budgetPlots = [
   {
-    title:"Signature FarukhNagar",link:"/signature-farukhnagar-plots/",image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/budgetplots/faruknagar.webp"
+    title: "Signature FarukhNagar", link: "/signature-farukhnagar-plots/", image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/budgetplots/faruknagar.webp"
   },
   {
-    title:"Signature Sidhrawali / NH-8",link:"/signature-global-plots/",image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/budgetplots/colors.jpg"
+    title: "Signature Sidhrawali / NH-8", link: "/signature-global-plots/", image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/budgetplots/colors.jpg"
   },
   {
-    title:"BPTP Limited",link:"/bptp-plots-gurugram/",image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/budgetplots/bptp.webp"
+    title: "BPTP Limited", link: "/bptp-plots-gurugram/", image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/budgetplots/bptp.webp"
   },
   {
-    title:"ORRIS Group",link:"/orris-plots-gurugram/",image:"https://d16gdc5rm7f21b.cloudfront.net/100acre/budgetplots/Orris.jpg"
+    title: "ORRIS Group", link: "/orris-plots-gurugram/", image: "https://d16gdc5rm7f21b.cloudfront.net/100acre/budgetplots/Orris.jpg"
   },
   // {
   //   title:"JMS Group",link:"/jms-plots-gurugram/",image:"../../Images/jms.jpg"
@@ -102,17 +102,17 @@ export const budgetPlots = [
 ];
 
 export const Recommendedreordered = [
-  "Shapoorji Pallonji Dualis" ,
+  "Shapoorji Pallonji Dualis",
   "Experion Windchants Nova",
-  "Signature Global Cloverdale SPR" ,
+  "Signature Global Cloverdale SPR",
   "Experion The Trillion",
 ];
 
 export const DesiredLuxuryOrder = [
-"Experion One 42",
-"Experion Windchants Nova",
-"Godrej Miraya",
-"DLF Privana North",
-"Shapoorji Pallonji Dualis",
-"Trevoc Royal Residences",
+  "Experion One 42",
+  "Experion Windchants Nova",
+  "Godrej Miraya",
+  "DLF Privana North",
+  "Shapoorji Pallonji Dualis",
+  "Trevoc Royal Residences",
 ]
