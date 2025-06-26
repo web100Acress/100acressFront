3ad856a7034e07d30507307c3ab53bf03f59edb4
ujjwalxoxo <ujjwal100acress@gmail.com>
2025-06-26 18:02:31 +0530

    Residential Property name change

--- src/Components/HomePageComponents/ResidentialProjects.jsx
+++ src/Components/HomePageComponents/ResidentialProjects.jsx
@@ -16,22 +16,22 @@ const ResidentialProjects = () => {
       <Helmet>
         <meta
           name="description"
-          content="Discover top residential properties for sale or rent in the best locations. Find your dream home today at 100acress."
+          content="Discover top Residential Property for sale or rent in the best locations. Find your dream home today at 100acress."
         />
-        <meta property="og:title" content="Explore Residential Properties in Prime Locations" />
+        <meta property="og:title" content="Explore Residential Property in Prime Locations" />
         <meta property="og:site_name" content="100acress" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://100acress-media-bucket.s3.ap-south-1.amazonaws.com/100acre/logo/logo.webp" />
         <meta property="og:url" content="https://www.100acress.com/property/residential/" />
-        <meta property="og:description" content="Discover top residential properties for sale or rent in the best locations. Find your dream home today at 100acress." />
-        <meta property="og:keywords" content="Residential Properties" />
-        <meta name="twitter:title" content="Explore Residential Properties in Prime Locations" />
-        <meta name="twitter:description" content="Discover top residential properties for sale or rent in the best locations. Find your dream home today at 100acress." />
+        <meta property="og:description" content="Discover top Residential Property for sale or rent in the best locations. Find your dream home today at 100acress." />
+        <meta property="og:keywords" content="Residential Property" />
+        <meta name="twitter:title" content="Explore Residential Property in Prime Locations" />
+        <meta name="twitter:description" content="Discover top Residential Property for sale or rent in the best locations. Find your dream home today at 100acress." />
         <meta name="twitter:url" content="https://twitter.com/100acressdotcom" />
         <meta name="twitter:card" content="summary" />
 
         <title>
-          Explore Residential Properties in Prime Locations
+          Explore Residential Property in Prime Locations
         </title>
         <link
           rel="canonical"
@@ -39,8 +39,8 @@ const ResidentialProjects = () => {
         />
       </Helmet>
       <CommonInside
-      title="RESIDENTIAL PROPERTIES"
-      details="Residential properties encompass a diverse range of housing options
+      title="Residential Property"
+      details="Residential Property encompass a diverse range of housing options
           tailored to meet varying lifestyle needs. From single-family homes to
           condominiums, townhouses, and apartments, each type offers unique
           features and amenities."
